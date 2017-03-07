48499ace86f22069f918f7eea55166345afafc12
nairsk <Surya@Mobiuso-iMac5.local>
2017-03-07 13:59:33 +0530

    Initial commit

---
+++ desktop/src/com/gdx/phils/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.phils.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.phils.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
