fdb4270264db98479121a3f636385bc2c97d8a78
akshay201190 <akshay201190@gmail.com>
2017-04-06 19:22:19 +0530

    Comes back to normal size

--- desktop/src/com/gdx/phils/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/phils/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.gdx.phils.MyGdxGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = config.height = 100000;
+        config.width = config.height = 500;
         new LwjglApplication(new MyGdxGame(), config);
     }
 }
